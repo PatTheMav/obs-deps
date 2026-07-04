8a25b0ae44993e77532603ba88fa73dbe4c90248
Service Checker <commits@obsproject.com>
2026-07-04 23:24:33 +0200

    rtmp-services: Remove defunct servers/services

--- deps.macos/90-sparkle.zsh
+++ deps.macos/90-sparkle.zsh
@@ -2,7 +2,7 @@ autoload -Uz log_debug log_error log_info log_status log_output
 
 ## Dependency Information
 local name='Sparkle'
-local version='2.9.2'
+local version='2.9.7'
 local url='https://github.com/sparkle-project/Sparkle/releases/download/2.9.2/Sparkle-2.9.2.tar.xz'
 local hash="${0:a:h}/checksums/Sparkle-2.9.2.tar.xz.sha256"
 
